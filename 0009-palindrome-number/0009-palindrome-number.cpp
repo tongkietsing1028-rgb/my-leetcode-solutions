41debdd17dd93a1fa19bef6096af5ed61941fd12
Tong Kiet Sing <tongkietsing1028@gmail.com>
2026-05-02 11:23:28 +0800

    Time: 0 ms (100%), Space: 8.6 MB (37.29%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -3,11 +3,11 @@ public:
     bool isPalindrome(int x) {
         if(x < 0 || (x % 10 == 0 && x!=0))return false;
         int res = 0;
-        while(res<x)
+        while(x>res)
         {
             res = res*10 + x%10;
             x /= 10;
         }
-        return res == x || res /10 == x;
+        return x == res || x == res/10;
     }
 };
