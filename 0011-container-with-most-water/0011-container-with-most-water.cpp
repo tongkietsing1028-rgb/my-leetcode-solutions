aa333499a37136eadab4dc26584ec5097f27269c
Tong Kiet Sing <tongkietsing1028@gmail.com>
2026-05-03 09:36:06 +0800

    Time: 0 ms (100%), Space: 62.9 MB (77.83%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int result = 0;
+        int l = 0, r = height.size()-1;
+        while(l<r)
+        {
+            result = max(result,min(height[l],height[r])*(r-l));
+            if(height[l]<height[r])l++;
+            else r--;
+        }
+        return result;
+    }
+};
