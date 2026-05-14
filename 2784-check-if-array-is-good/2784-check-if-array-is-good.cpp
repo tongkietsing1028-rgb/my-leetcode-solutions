8978556801d4276d6155fa08c2820ad71c50b724
Tong Kiet Sing <tongkietsing1028@gmail.com>
2026-05-14 17:15:36 +0800

    Time: 1 ms (25.58%), Space: 26.4 MB (73.39%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
@@ -3,11 +3,11 @@ public:
     bool isGood(vector<int>& nums) {
         int n = nums.size();
         sort(nums.begin(),nums.end());
-        int max = nums.back();
-        for(int i =0;i<max;i++)
+        int mx = nums.back();
+        for(int i =0;i<mx;i++)
         {
             if(nums[i]!= i+1)return false;
         }
-        return max == n-1;
+        return mx == n-1;
     }
 };
